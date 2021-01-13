57284d8fdeb34d046691a5bd1c25e9a7fd3ba936
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2021-01-13 16:29:38 +0200

    fix delete button bug

--- js/profile.js
+++ js/profile.js
@@ -6,28 +6,28 @@ sortBy.addEventListener('change', function () {
     loadArticles(true, this.value);
 });
 
+function appendDeleteButtons() {
+    const userArticles = document.querySelectorAll('.article');
+    for (const userArticle of userArticles) {
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = 'Delete';
+        deleteButton.classList.add('delete-button');
+        deleteButton.addEventListener('click', function () {
+            fetch('/article?username=' + username + '&accessToken=' + accessToken, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({ articleId: userArticle.id.slice(9) })
+            }).then(() => document.querySelector('#' + userArticle.id).parentNode.innerHTML = '');
+        })
+        userArticle.appendChild(deleteButton);
+    }
+}
+
 loadArticles(true, 'most-recent')
-    .then(() => {
-        const userArticles = document.querySelectorAll('.article');
-        for (const userArticle of userArticles) {
-            const deleteButton = document.createElement('button');
-            deleteButton.innerHTML = 'Delete';
-            deleteButton.classList.add('delete-button');
-            deleteButton.addEventListener('click', function () {
-                fetch('/article?username=' + username + '&accessToken=' + accessToken, {
-                    method: 'DELETE',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/json'
-                    },
-                    body: JSON.stringify({ articleId: userArticle.id.slice(9) })
-                }).then(() => {
-                    loadArticles(true, 'most-recent');
-                });
-            })
-            userArticle.appendChild(deleteButton);
-        }
-    });
+    .then(appendDeleteButtons);
 
 
 
